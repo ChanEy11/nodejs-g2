3e2a5bd0f490ff8e3e5a9cd193da11499b6b7124
ChanEy <chaneyhengc@gmail.com>
2022-08-25 09:04:24 +0700

    nodejs-g2

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 
+// const db = mysql.createConnection({
+//     host : "localhost",
+//     database : "nodejs-g2",
+//     user : "root",
+//     password : ""
+// })
+
 const db = mysql.createConnection({
-    host : "localhost",
-    database : "nodejs_g2",
-    user : "root",
-    password : ""
+    host : "bn3d9eb0e4zyxtwbpqqg-mysql.services.clever-cloud.com",
+    database : "bn3d9eb0e4zyxtwbpqqg",
+    user : "uqidhymqrp0kfpe4",
+    password : "s9C1GO313q0vZl72SKm6"
 })
 
 db.connect();
